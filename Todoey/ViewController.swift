fabbde17c505b09fa73929015b35aab122a60d52
Nate Meharg <nate.meharg@life.church>
2019-02-08 11:13:15 -0600

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Nate Meharg on 2/8/19.
+//  Copyright © 2019 Nathaniel Meharg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

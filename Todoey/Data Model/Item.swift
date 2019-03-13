a37bf91ca89e39c3ca630936473a2d2ce240bb5e
Nate Meharg <nate.meharg@life.church>
2019-03-13 13:53:50 -0500

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Nate Meharg on 3/11/19.
+//  Copyright © 2019 Nathaniel Meharg. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+}
